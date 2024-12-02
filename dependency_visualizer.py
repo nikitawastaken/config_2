3b5e15c41ad30ff79058dc8a6c0907ad22b8fac3
nikitawastaken <59087849+nikitawastaken@users.noreply.github.com>
2024-12-02 14:59:23 +0300

    add main function, finish up functionality

--- dependency_visualizer.py
+++ dependency_visualizer.py
@@ -1,4 +1,6 @@
+import os
 import gzip
+import argparse
 from graphviz import Digraph
 
 
@@ -56,3 +58,42 @@ def visualize_dependency_graph(graph, output_path, graphviz_program):
             dot.edge(package, dep)
     
     dot.render(output_path, cleanup=True)
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Визуализатор графа зависимостей пакетов Ubuntu без сторонних утилит."
+    )
+    parser.add_argument("package", help="Имя анализируемого пакета")
+    parser.add_argument(
+        "--packages-file",
+        required=True,
+        help="Путь к файлу Packages или Packages.gz"
+    )
+    parser.add_argument(
+        "--program",
+        default="dot",
+        help="Программа для визуализации графов (по умолчанию dot)"
+    )
+    parser.add_argument(
+        "--output",
+        default="dependencies",
+        help="Путь для сохранения результата"
+    )
+    args = parser.parse_args()
+
+    if not os.path.exists(args.packages_file):
+        print(f"Файл {args.packages_file} не найден.")
+        return
+
+    try:
+        dependencies = parse_packages_file(args.packages_file)
+        graph = build_dependency_graph(args.package, dependencies)
+        visualize_dependency_graph(graph, args.output, args.program)
+        print(f"Граф зависимостей пакета {args.package} успешно сохранен в {args.output}.png.")
+    except Exception as e:
+        print(f"Ошибка: {e}")
+
+
+if __name__ == "__main__":
+    main()
