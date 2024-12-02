2b86025d300925d049e1e1cdfabee3db1ea439d7
nikitawastaken <59087849+nikitawastaken@users.noreply.github.com>
2024-12-02 14:39:38 +0300

    initial commit

---
+++ dependency_visualizer.py
@@ -0,0 +1,43 @@
+import gzip
+
+
+def parse_packages_file(packages_file_path):
+    """
+    Парсит файл Packages и возвращает словарь зависимостей.
+    """
+    dependencies = {}
+    current_package = None
+
+    with gzip.open(packages_file_path, 'rt') if packages_file_path.endswith('.gz') else open(packages_file_path, 'r') as f:
+        for line in f:
+            line = line.strip()
+            if line.startswith("Package:"):
+                current_package = line.split(":", 1)[1].strip()
+                dependencies[current_package] = []
+            elif line.startswith("Depends:") and current_package:
+                deps = line.split(":", 1)[1].strip()
+                dep_list = [dep.split()[0] for dep in deps.split(",")]
+                dependencies[current_package].extend(dep_list)
+
+    return dependencies
+
+
+def build_dependency_graph(package_name, dependencies, graph=None, seen=None):
+    """
+    Рекурсивно строит граф зависимостей для пакета.
+    """
+    if graph is None:
+        graph = {}
+    if seen is None:
+        seen = set()
+
+    if package_name in seen or package_name not in dependencies:
+        return graph
+
+    seen.add(package_name)
+    graph[package_name] = dependencies.get(package_name, [])
+
+    for dep in graph[package_name]:
+        build_dependency_graph(dep, dependencies, graph, seen)
+
+    return graph
